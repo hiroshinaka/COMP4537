1456e314b408d9f4321a9b10b737c587579b6d9e
Hiroshi <mnakasone3@my.bcit.ca>
2025-09-02 16:13:50 -0700

    index update

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const button_container = document.getElementsByClassName('.button-container');
+const button_creator = document.getElementsByClassName('create-buttons');
+
+//Value for number of buttons to create
+let storedValue = 0
