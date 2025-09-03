c1714043a3aca1c997c7a9be31ae84c15ad689f5
Hiroshi <mnakasone3@my.bcit.ca>
2025-09-02 22:08:47 -0700

    minor changes

--- js/GameButton.js
+++ js/GameButton.js
@@ -1,13 +1,21 @@
 class GameButton{
-    constructor(){
+    constructor(id){
+        this.id = this.id;
+        this.isClickable = false;
+
         this.buttonElement = document.createElement("button");
         this.buttonElement.style.width = "10em";
         this.buttonElement.style.height = "5em";
         this.buttonElement.style.position = "absolute";
         this.buttonElement.style.display = "none";
-        this.buttonElement.style.backgroundColor = 
-        this.isClickable = false;
+        this.buttonElement.style.backgroundColor = this.randomColor();
+
+        this.buttonElement.textContent = id;
         document.body.appendChild(this.buttonElement);
+    }
+    randomColor(){
+        const h = Math.floor(Math.random() * 360);
+        return `hsl(${h}, 70%, 50%)`;
     }
     setClickable(isClickable){
         this.isClickable = isClickable;
@@ -16,5 +24,13 @@ class GameButton{
 
     hideText(){
         this.buttonElement.textContent = "";
+    }
+    showText(){
+        this.buttonElement.textContent = this.id;
+    }
+    moveTo(x,y){
+        this.buttonElement.style.left = `${x}px`;
+        this.buttonElement.style.top = `${y}px`;
+        this.buttonElement.style.display = "block";
     }
 }
