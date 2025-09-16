cde6886a247e792f90fa2d3a5b6f246205d8e78f
Hiroshi Nakasone <122339795+hiroshinaka@users.noreply.github.com>
2025-09-16 10:42:51 -0700

    Revise documentation for Note.from method

--- COMP4537/labs/1/js/note.js
+++ COMP4537/labs/1/js/note.js
@@ -23,12 +23,8 @@ export class Note {
   }
 
   /**
-   * Creates a new Note object from a given object.
+   * Creates a New Note from a plain obj. Used for converting stored obj in localstorage back to Note obj
    * @param {*} obj - The object to create a Note from.
-   * If the object has an 'id' property of type string, it is used as the id of the Note.
-   * If the object has a 'text' property of type string, it is used as the text of the Note.
-   * If the object does not have an 'id' property of type string, a random uuid is used as the id of the Note.
-   * If the object does not have a 'text' property of type string, an empty string is used as the text of the Note.
    * @returns {Note} - The new Note object.
    **/
   static from(obj) {
